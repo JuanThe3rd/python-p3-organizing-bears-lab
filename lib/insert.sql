51eb904b485fcf60701a14ec7ab3e087066af1e2
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-11 15:21:25 -0700

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Black", "Obnoxiously Loud", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "White", "Relaxing Most of Time", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "White", "Very Stern and Bossy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "Black", "Obnoxiously Loud with Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Brown", "Very Bitter and Mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 11, "F", "White", "Mom of the Group", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "Brown", "Newborn Baby", 1);
